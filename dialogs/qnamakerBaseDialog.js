4b02bd15b4c252334f6de906a04da1aba138ea63
Kaspars Beteris <kaspars.beteris@gmail.com>
2019-12-03 23:22:05 +0200

    Updating answer if question not found

--- dialogs/qnamakerBaseDialog.js
+++ dialogs/qnamakerBaseDialog.js
@@ -12,7 +12,7 @@ const { QnACardBuilder } = require('../utils/qnaCardBuilder');
 // Default parameters
 const DefaultThreshold = 0.3;
 const DefaultTopN = 3;
-const DefaultNoAnswer = 'No QnAMaker answers found.';
+const DefaultNoAnswer = 'Sorry, no answers found. Try phrasing it differently, please.';
 
 // Card parameters
 const DefaultCardTitle = 'Did you mean:';
